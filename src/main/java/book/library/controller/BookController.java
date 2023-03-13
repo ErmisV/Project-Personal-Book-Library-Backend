e4fb87fd8434cfb34a9f895361cc8c9371543864
ErmisV <ermisvasilaros@gmail.com>
2023-03-13 21:34:59 +0200

    fixed CORS , Back-front connected

--- src/main/java/book/library/controller/BookController.java
+++ src/main/java/book/library/controller/BookController.java
@@ -3,6 +3,7 @@ package book.library.controller;
 import book.library.model.Book;
 import book.library.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1/")
 public class BookController {
 
@@ -17,7 +19,6 @@ public class BookController {
     private BookRepository bookRepository;
 
     //get all books
-
     @GetMapping("/books")
     public List<Book> getAllBooks(){
         return bookRepository.findAll();
