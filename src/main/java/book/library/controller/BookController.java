8fede665aea7467a94eb3b5495a2604e820e0611
ErmisV <ermisvasilaros@gmail.com>
2023-03-16 18:22:19 +0200

    added REST API for : Update Book, Delete Book // Version 1.0 Finished

--- src/main/java/book/library/controller/BookController.java
+++ src/main/java/book/library/controller/BookController.java
@@ -7,29 +7,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin (origins = "http://localhost:4200")
 @RequestMapping("/api/v1/")
 public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
 
-    //get all books REST API
+    //get All books (List)(G) /REST API
     @GetMapping("/books")
     public List<Book> getAllBooks(){
         return bookRepository.findAll();
     }
 
-    //Book REST API
+    //Create Book (G) /REST API
     @PostMapping("/books")
     public Book createBook(@RequestBody Book book){
         return bookRepository.save(book);
     }
 
-    //Get Book by id REST API
+    //Get Book by id /REST API
     @GetMapping("/books/{id}")
     public ResponseEntity<Book> getBookById(@PathVariable long id)
     {
@@ -37,4 +37,36 @@ public class BookController {
                 .orElseThrow(() -> new ResourceNotFoundException("Book does not exist with id : " + id));
         return ResponseEntity.ok(book);
     }
+
+    //Update Book by id /REST API
+    @PutMapping("/books/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id,@RequestBody Book bookDetails){
+
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book does not exist with id : " + id));
+
+        book.setBookName(bookDetails.getBookName());
+        book.setBookAuthor(bookDetails.getBookAuthor());
+        book.setBookPages(bookDetails.getBookPages());
+        book.setBookTag(bookDetails.getBookTag());
+
+        Book updatedBook = bookRepository.save(book);
+        return  ResponseEntity.ok(updatedBook);
+    }
+
+    //Delete Book by id /REST API
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteBook(@PathVariable Long id){
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book does not exist with id:" + id));
+        bookRepository.delete(book);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return  ResponseEntity.ok(response);
+    }
+
+
 }
+
+
+
